09b9c9eeb5000e062698e1d8bd2f7f259baf1194
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-01-14 11:21:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import axios from 'axios';
+
+
+
+</script>
+
+<template>
+  <div>
+    ciao
+  </div>
+</template>
+
+<style lang="scss">
+@use './bootstrap/scss/bootstrap.scss' as *;
+@use './styles/general.scss' as *;
+@use './styles/partials/variables.scss' as *;
+
+</style>
